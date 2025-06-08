960d3f4def8c5998c344da8df61b143e0b036f6d
crimson206 <crimson206@gmail.com>
2025-06-08 14:37:10 +0200

    added dependencies

--- scripts/tagging.sh
+++ scripts/tagging.sh
@@ -1,2 +1,2 @@
-git tag -a v0.1.0 -m "Initial release v0.1.0" && git push origin v0.1.0
 git tag -d v0.1.0 && git push origin :refs/tags/v0.1.0
+git tag -a v0.1.0 -m "Initial release v0.1.0" && git push origin v0.1.0
